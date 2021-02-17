1ce89cb7358e46705d175ef713cf913f282a2931
cheonseyoung <pocu1407@gmail.com>
2021-02-17 23:20:18 +0900

    commit practice

--- post_api/post_api.py
+++ post_api/post_api.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
-@app.route('/test', methods=['POST'])
-def post():
-    a = request.form['a']
-    b = request.form['b']
-    return a+b
-
+##http://127.0.0.1:5000/test
 
 @app.route('/test')
-def post():
+def post1():
     return render_template('post.html')
+
+##http://127.0.0.1:5000/calculate
+
+@app.route('/calculate', methods=['POST'])
+def post2():
+    a = request.form['a']
+    b = request.form['b']
+    a_plus_b = int(a) + int(b)
+    return render_template('add.html', c=a_plus_b)
 
 
 if __name__ == '__main__':
